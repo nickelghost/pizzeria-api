8276837db5dd5ebb406dc4feba2e42cfea3b9cd5
Gabriel Karczewski <nickelghost@outlook.com>
2020-10-23 23:43:50 +0200

    Add error handling and req validation

--- api/controllers/CarouselItemsAdminController.ts
+++ api/controllers/CarouselItemsAdminController.ts
@@ -1,39 +1,69 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import * as yup from 'yup';
 
 import setContentRangeHeader from '../lib/setContentRangeHeader';
 import { S3Uploader } from '../lib/uploader/s3';
 import Uploader from '../lib/uploader/uploader';
+import CarouselItem from '../models/CarouselItem';
 import CarouselItemsService from '../services/CarouselItemsService';
+
+const schemas = {
+  create: yup.object().shape({
+    title: yup.string().required(),
+    description: yup.string(),
+    picture: yup.string().required(),
+    destinationUrl: yup.string(),
+  }),
+};
 
 class CarouselItemsAdminController {
   service = new CarouselItemsService();
   uploader: Uploader = new S3Uploader();
 
-  index = async (req: Request, res: Response) => {
-    const carouselItems = await this.service.findAll('*');
+  index = async (req: Request, res: Response, next: NextFunction) => {
+    let carouselItems: CarouselItem[];
+    try {
+      carouselItems = await this.service.findAll('*');
+    } catch (e) {
+      return next(e);
+    }
     const { length } = carouselItems;
     setContentRangeHeader(res, 0, length, length);
     res.send(carouselItems);
   };
 
-  create = async (req: Request, res: Response) => {
+  create = async (req: Request, res: Response, next: NextFunction) => {
+    const reqValid = await schemas.create.isValid(req.body);
+    if (!reqValid) {
+      res.status(400).send();
+      return next();
+    }
     const { title, description, picture, destinationUrl } = req.body;
-    const { url: pictureUrl, key: pictureKey } = await this.uploader.add(
-      picture,
-    );
-    const carouselItem = await this.service.create({
-      title,
-      description,
-      pictureUrl,
-      pictureKey,
-      destinationUrl,
-    });
-    res.send(carouselItem);
+    try {
+      const { url: pictureUrl, key: pictureKey } = await this.uploader.add(
+        picture,
+      );
+      const carouselItem = await this.service.create({
+        title,
+        description,
+        pictureUrl,
+        pictureKey,
+        destinationUrl,
+      });
+      res.send(carouselItem);
+    } catch (e) {
+      return next(e);
+    }
   };
 
-  destroy = async (req: Request, res: Response) => {
+  destroy = async (req: Request, res: Response, next: NextFunction) => {
     const { id } = req.params;
-    const carouselItem = await this.service.destroy(id);
+    let carouselItem: CarouselItem;
+    try {
+      carouselItem = await this.service.destroy(id);
+    } catch (e) {
+      return next(e);
+    }
     res.send(carouselItem);
   };
 }
--- api/controllers/CarouselItemsController.ts
+++ api/controllers/CarouselItemsController.ts
@@ -1,4 +1,6 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+
+import CarouselItem from '../models/CarouselItem';
 import CarouselItemsService from '../services/CarouselItemsService';
 
 const FIELDS = ['title', 'description', 'pictureUrl', 'destinationUrl'];
@@ -6,8 +8,13 @@ const FIELDS = ['title', 'description', 'pictureUrl', 'destinationUrl'];
 class CarouselItemsController {
   service = new CarouselItemsService();
 
-  index = async (req: Request, res: Response) => {
-    const carouselItems = await this.service.findAll(FIELDS);
+  index = async (req: Request, res: Response, next: NextFunction) => {
+    let carouselItems: CarouselItem[];
+    try {
+      carouselItems = await this.service.findAll(FIELDS);
+    } catch (e) {
+      return next(e);
+    }
     res.send(carouselItems);
   };
 }
