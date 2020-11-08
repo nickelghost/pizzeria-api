08b6cf4db26bbb83cbbe61020c9dc8b4f01df4a9
Gabriel Karczewski <nickelghost@outlook.com>
2020-11-08 23:39:26 +0100

    Add fallbacks for S3 config

--- api/config.ts
+++ api/config.ts
@@ -6,11 +6,11 @@ const config = {
   port: Number(process.env.APP_PORT) || 3000,
   bodySizeLimit: process.env.APP_BODY_SIZE_LIMIT || '20mb',
   s3: {
-    bucket: process.env.S3_BUCKET,
-    region: process.env.S3_REGION,
-    endpoint: process.env.S3_ENDPOINT,
-    accessKeyId: process.env.S3_ACCESS_KEY_ID,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    bucket: process.env.S3_BUCKET || '',
+    region: process.env.S3_REGION || '',
+    endpoint: process.env.S3_ENDPOINT || '',
+    accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
   },
 };
 
